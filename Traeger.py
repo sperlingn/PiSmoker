eb5b165c27d28239e453eaf13a33952fb3ddff82
sperlingn <nicholas.sperling@utoledo.edu>
2017-01-12 20:45:10 -0500

    Update TODO, begin trager class rework, including option for GPIO inversion.
    Signed-off-by: sperlingn <nicholas.sperling@utoledo.edu>

--- Traeger.py
+++ Traeger.py
@@ -5,26 +5,30 @@ import time, logging, logging.config
 logging.config.fileConfig('/home/pi/PiSmoker/logging.conf')
 logger = logging.getLogger(__name__)
 class Traeger:
-	
-	def __init__(self, Relays):
-		self.Relays = Relays
-		self.ToggleTime = {}
-		self.Initialize()
-		
-	def Initialize(self):
-		GPIO.setwarnings(False)
-		GPIO.setmode(GPIO.BCM)
-		for k in self.Relays.keys():
-			GPIO.setup(self.Relays[k],GPIO.OUT)
-			self.SetState(k,0)
-			self.ToggleTime[k] = time.time()
+    Relays = []
+    ToggleTime = { }
+    GPIO_Invert = False
 
-	def GetState(self,Relay):
-		return not GPIO.input(self.Relays[Relay])
-	
-	def SetState(self,Relay,state):
-		if not (self.GetState(Relay) == state):
-			logger.info('Toggling %s: %d',Relay,state)
-			self.ToggleTime[Relay] = time.time()
-		GPIO.output(self.Relays[Relay],not state)
-		
+    def __init__(self, relays, invert=False):
+        self.Relays = relays
+        self.ToggleTime = dict()
+        self.GPIO_Invert = invert
+        self.Initialize()
+
+    def Initialize(self):
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        for k in self.Relays.keys():
+            GPIO.setup(self.Relays[k],GPIO.OUT)
+            self.SetState(k,0)
+            self.ToggleTime[k] = time.time()
+
+    def GetState(self, relay_id):
+        return not GPIO.input(self.Relays[relay_id])
+
+    def SetState(self, relay_id, state):
+        if not (self.GetState(relay_id) == state):
+            logger.info('Toggling %s: %d', relay_id, state)
+        self.ToggleTime[relay_id] = time.time()
+        GPIO.output(self.Relays[relay_id], state != self.GPIO_Invert)
+
